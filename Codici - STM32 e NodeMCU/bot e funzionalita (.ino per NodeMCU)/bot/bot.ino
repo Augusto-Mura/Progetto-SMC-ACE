2d35aaebf01223f100c893d54ac19be06c2919aa
robberuda <robberuda@gmail.com>
2020-09-29 16:34:15 +0200

    CODICE FINALE NodeMCU
    ricommentato e pronto per la presentazione

--- Codici - STM32 e NodeMCU/bot e funzionalita (.ino per NodeMCU)/bot/bot.ino
+++ Codici - STM32 e NodeMCU/bot e funzionalita (.ino per NodeMCU)/bot/bot.ino
@@ -5,7 +5,7 @@
 #include <WiFiClientSecure.h>
 #include <UniversalTelegramBot.h>
 // Library for interacting with the Telegram API
-// Search for "Telegegram" in the Library manager and install UniversalTelegramBot by Brian Lough
+// Search for "Telegram" in the Library manager and install UniversalTelegramBot by Brian Lough
 // https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
 #include <ArduinoJson.h>
 // Library used for parsing Json from the API responses
@@ -16,98 +16,82 @@
 //-------------------------------------------------------------------------------------------------------------------------
 //--------------------------------------------------COSTANT&DEFINITION-----------------------------------------------------
 //-------------------------------------------------------------------------------------------------------------------------
-//------- Replace the following! ------
-char ssid[] = "FASTWEBRU";         // your network SSID (name)
-char password[] = "fastwebruda"; // your network password
-#define TELEGRAM_BOT_TOKEN "1365422019:AAEAjwqsaY3zhMjLUULraW3KsWCXGu5SPwQ"  // Your Telegram Bot Token 
+char ssid[] = "FASTWEBRU";         // network SSID (name)
+char password[] = "fastwebruda"; // network password
+#define TELEGRAM_BOT_TOKEN "1365422019:AAEAjwqsaY3zhMjLUULraW3KsWCXGu5SPwQ"  // Telegram Bot Token 
 
-#define wled D4
-#define interruptpin D5 //pin di interrupt
-#define wrn0 D1 //bit led n1
+#define wled D4 // WiFi status led
+#define alertPin D5 //pin di alert
+#define wrn0 D1 //bit led n1, o bit per indicare il codice dello step
 #define wrn1 D6 //bit led n2
 #define wrn2 D7 //bit led n3
 #define wrn3 D2 //bit n1 di tipologia di guasto 
 #define wrn4 D8 //bit n2 di tipologia di guasto 
 
-byte warning = 0x01;   // es byte = 00000001
-int navvertimento = 0; //quanti avvertimenti sono  arrivati
-int cntEvent = 0; 
-int polling = 0; //segnale che se passa alto indica un evento di warning    
-String idallarm = "null";//id al quale mandare gli allarmi, si setta nel void setup
+byte warning = 0x00;   // stringa di bit che compone il messaggio
+int polling = 0; //variabile che se passa alto indica un evento di warning    
+String idallarm = "null";//id al quale mandare gli allarmi, viene deciso nel void setup
 
-String ids[10];
-//es:
-// ids [0] = 123456789  //roberto 
-// ids [1] = 987783423  //danjo
-//------- ---------------------- ------
+String ids[10]; // array di id, ogni id indica una chat
 
-// This is the Wifi client that supports HTTPS
-WiFiClientSecure client;
+
+WiFiClientSecure client;// Wifi client che supporta HTTPS
 UniversalTelegramBot bot(TELEGRAM_BOT_TOKEN, client);
-
-int delayBetweenChecks = 1000;
-unsigned long lastTimeChecked;   //last time messages' scan has been done
-
-unsigned long lightTimerExpires;
-boolean lightTimerActive = false;
 
 
 //-------------------------------------------------------------------------------------------------------------------------
 //-------------------------------------------------------FUNZIONI----------------------------------------------------------
 //-------------------------------------------------------------------------------------------------------------------------
-void wifiConnection(){   //LASCIATE COSì QUESTA FUNZIONE, NON TOCCARE!
-  // Set WiFi to station mode and disconnect from an AP if it was Previously connected
+void wifiConnection(){   
+  // WiFi impostato in station mode e disconnetti da altri router(solo se precedentemente connesso)
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
   delay(100);
-    // attempt to connect to Wifi network:
+  // connessione al WiFi:
   Serial.print("Connecting Wifi: ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    digitalWrite(wled, HIGH);
+  while (WiFi.status() != WL_CONNECTED) { //si esce dal while quando la connessione è avvunuta con successo
+    digitalWrite(wled, HIGH); //led status WiFi spento
     Serial.print(".");
     delay(500);
   }
-  digitalWrite(wled, LOW);
+  digitalWrite(wled, LOW);//led status WiFi acceso 
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-  // Only required on 2.5 Beta
+  Serial.println(WiFi.localIP()); //stampa l'ip locale 
   client.setInsecure();
-  // longPoll keeps the request to Telegram open for the given amount of seconds if there are no messages
-  // This hugely improves response time of the bot, but is only really suitable for projects
-  // where the the initial interaction comes from Telegram as the requests will block the loop for
-  // the length of the long poll
-  bot.longPoll = 60;
+  
+  bot.longPoll = 60;// costante utilizzata per migliorare i tempi di risposta del bot telegram
   }
 
-void interpretaEavverti(String id){//funzione che capisce che combinaziona è arrivata e manda il messaggio a id
-    int guasto = (warning & 0x18)>>3; //shift codice guasto
-    int idLED = (warning & 0x07);
+void sendAlertTelegram(String id){//funzione che interpreta la combinazione e manda il messaggio a id
+    int guasto = (warning & 0x18)>>3; // filtra il codice di guasto (& 00011000), e lo shifta a destra di 3 pos
+    int idLED = (warning & 0x07); // filtra i 3 bit utilizzati per l'indice del led o per il codice dello STEP
     switch(guasto){      
         case(0):
-        {String txt = "GUASTO PERTINENTE nel led n° " + String(idLED+1);
+        {String txt = "GUASTO PERTINENTE nel LED n° "+ String(idLED+1);
         bot.sendMessage(String(id), txt);}
         break;        
         case(1):
-        {String txt = "GUASTO NON PERTINENTE nel led n° " + String(idLED+1);
+        {String txt = "GUASTO NON PERTINENTE nel LED n° " + String(idLED+1);
         bot.sendMessage(String(id), txt);}
         break;        
-        case(2): //00000010
-        bot.sendMessage(String(id), "FINE STEP (vai e riattacca tutto)");    //aggiungi cose da fare{}
+        case(2):
+        bot.sendMessage(String(id), "FINE STEP " + String(idLED) + "\n1)Premi il punsalte \n2)Cambia la corrente su i LED \n3)Premi nuovamente il pulsante"); 
+        // (usiamo idLED per indicare in che STEP siamo)
         break;        
         case(3):
-        bot.sendMessage(String(id), "TUTTI I LED SI SONO ROTTI");
+        bot.sendMessage(String(id), "FINE PROVA (tutti i LED hanno manifestato un guasto).");
         break;
         default:
-        bot.sendMessage(String(id), "ERRORE COMBINAZIONE");
+        bot.sendMessage(String(id), "ERRORE COMBINAZIONE warning");
         break;
     }
 }
 
-void readpin(){                   // CREA LA VARIABILE WEARNING LEGGENDO IL VALORE DEI BIT DEL LED E DEL GUASTO
+void readPin(){ //Funzione che crea la variabile warning leggendo i valori in input 
     bitWrite(warning, 0, digitalRead(wrn0));
     bitWrite(warning, 1, digitalRead(wrn1));
     bitWrite(warning, 2, digitalRead(wrn2));
@@ -121,26 +105,24 @@ void readpin(){                   // CREA LA VARIABILE WEARNING LEGGENDO IL VALO
 //-------------------------------------------------------------------------------------------------------------------------
 void setup() 
 {
-  Serial.begin(115200);
+  Serial.begin(115200); // apri il monitor seriale
   
-  // set IO 
+  // set Input e Output
   pinMode(wled, OUTPUT);
   digitalWrite(wled, HIGH);
   
-  pinMode(interruptpin, INPUT); //INTERRUPT PIN = D5
+  pinMode(alertPin, INPUT); //ALERT PIN = D5
   pinMode(wrn0, INPUT);   
   pinMode(wrn1, INPUT);
   pinMode(wrn2, INPUT);   
   pinMode(wrn3, INPUT);
 
-  ids[0] = "291655246" ; //robbe
-  ids[1] = "196098030" ; //augu
-  ids[2] = "134892235" ; //danjo
-  ids[3] = "-1001219067722"; //canale per gli allarmi sulle prove
-  ids[5] = "000000000" ; //zeri
-  idallarm = ids[3];    // <-----------------------------------------settare qui a quale id bisogna mandare gli allarmi
-  wifiConnection(); //esegue tutte le istruzioni che ho commentato sopra
-  bot.sendMessage(String(idallarm), "BOT ATTIVO");
+  ids[0] = "291655246" ; // roberto (usato in debug)
+  ids[1] = "-1001219067722"; // canale per gli allarmi sulle prove
+  ids[2] = "000000000" ; // zeri (usato in debug)
+  idallarm = ids[1];    // settare qui a quale id bisogna mandare gli allarmi
+  wifiConnection(); //connessione al WiFI
+  bot.sendMessage(String(idallarm), "BOT ATTIVO."); //primo messaggio che indica che il bot è in funzione
 }
 
 
@@ -149,12 +131,12 @@ void setup()
 //-------------------------------------------------------------------------------------------------------------------------
 void loop() 
 {
-  delay(100); //100 millisecondi
-  polling = digitalRead(interruptpin);
+  delay(100); // 100 millisecondi
+  polling = digitalRead(alertPin);
   
   if (polling) {
-    readpin();
-    interpretaEavverti(String(idallarm));
-    delay(1100); //metto 5 secondi per fare i test a mano, poi ri metteremo 1100 millisecondi
+    readPin();
+    sendAlertTelegram(String(idallarm));
+    delay(1000); // 1 secondo per evitare di leggere più volte lo stesso alert dall'STM32
     }
 }
